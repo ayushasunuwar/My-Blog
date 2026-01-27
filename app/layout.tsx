ebcfbf5af34b324c97143cd920db74b801e1b192
ayushasunuwar <aisharodriguez91@gmail.com>
2026-01-27 19:26:55 +0545

    Created Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${outfit} antialiased`}
+        className={`${outfit.className} antialiased`}
       >
         {children}
       </body>
