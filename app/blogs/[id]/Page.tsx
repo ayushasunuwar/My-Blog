c200bc55cd3876e87de39a03d60ada662ef34f94
ayushasunuwar <aisharodriguez91@gmail.com>
2026-01-28 10:44:06 +0545

    Fixed some issues and created schema and modle

--- app/blogs/[id]/Page.tsx
+++ app/blogs/[id]/Page.tsx
@@ -1,10 +1,72 @@
-import React from 'react'
+'use client'
+import { useParams } from 'next/navigation'
+import { assets, blog_data } from "@/assets/assets"
+import { useEffect, useState } from "react"
+import Image from "next/image"
+import Footer from '@/components/Footer'
+import Link from 'next/link'
 
-const Page = ({params}) => {
+
+const Page = () => {
+ const params = useParams() // <-- hook to safely get dynamic params
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    const found = blog_data.find(blog => blog.id === Number(params.id))
+    if (found) setData(found)
+  }, [params.id])
+
+  if (!data) return null;
+
   return (
-    <div>
-        {params.id}
+    <>
+    <div className="bg-gray-200 py-5 px-5 md:px-12 lg:px-28 ">
+      <div className="flex justify-between items-center">
+        <Link href={'/'}>
+            <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto'/>
+        </Link>
+        
+        <button className="flex items-center gap-2 font-md py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_000000]">
+          Get Started <Image src={assets.arrow} alt=""/>
+          </button>
+      </div>
+
+      <div className="text-center my-24">
+        <h1 className='text-2xl sm:text-5xl font-semibold max-w-[700px] mx-auto'>{data.title}</h1>
+        <Image className='mx-auto mt-6 border border-white rounded-full' src={data.author_img} width={60} height={60} alt=''/>
+        <p className='mt-1 pb-2 text-lg max-w-[740px] mx-auto'>{data.author}</p>
+      </div>
     </div>
+
+    <div className='mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10'>
+        <Image className='border-4 border-white' src={data.image} width={1280} height={720} alt=''/>
+        <h1 className='my-8 text-[26px] font-semibold'>Introduction:</h1>
+        <p>{data.description}</p>
+        <h3 className='my-5 text-[18px] font-semibold'>Step 1: </h3>
+        <p className='my-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, asperiores?</p>
+        <p className='my-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, asperiores?</p>
+
+        <h3 className='my-5 text-[18px] font-semibold'>Step 2: </h3>
+        <p className='my-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, asperiores?</p>
+        <p className='my-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, asperiores?</p>
+
+        <h3 className='my-5 text-[18px] font-semibold'>Step 3: </h3>
+        <p className='my-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, asperiores?</p>
+        <p className='my-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, asperiores?</p>
+
+        <div className='my-24'>
+            <p className='text-black font-semibold my-4'>Share This Article on Social Media</p>
+            <div className='flex'>
+                <Image src={assets.facebook_icon} width={50} alt=''/>
+                <Image src={assets.twitter_icon} width={50} alt=''/>
+                <Image src={assets.googleplus_icon} width={50} alt=''/>
+            </div>
+        </div>
+ 
+    </div>
+    <Footer/>
+
+ </>
   )
 }
 
--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { blog_data } from "@/assets/assets"
 import BlogItem from '@/components/BlogItem'
 import { useState } from "react"
@@ -18,7 +20,7 @@ const BlogList = () => {
         <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
             {
                 blog_data.filter((item) => menu === "All" ? true : item.category === menu).map((item, index) => {
-                    return <BlogItem key={index} image={item.image} title={item.title} description={item.description} category={item.category}/>
+                    return <BlogItem key={index} id={item.id} image={item.image} title={item.title} description={item.description} category={item.category}/>
                 })}
         </div>
     </div>
